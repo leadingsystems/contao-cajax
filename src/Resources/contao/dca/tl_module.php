f22c594e7842a359497a7052244373e283ef3f7f
Daniel Bitsch <bitsch@leadingsystems.de>
2023-10-11 14:38:17 +0200

    convert indents

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,15 +1,15 @@
 <?php
 foreach ($GLOBALS['TL_DCA']['tl_module']['palettes'] as $paletteName => $palette)  {
-	if ($paletteName == '__selector__') {
-		continue;
-	}
-	$GLOBALS['TL_DCA']['tl_module']['palettes'][$paletteName] .= ';{ls_cajax_legend:hide},cajaxIdentifierString';
+    if ($paletteName == '__selector__') {
+        continue;
+    }
+    $GLOBALS['TL_DCA']['tl_module']['palettes'][$paletteName] .= ';{ls_cajax_legend:hide},cajaxIdentifierString';
 }
-			
+
 $GLOBALS['TL_DCA']['tl_module']['fields']['cajaxIdentifierString'] = array(
-	'exclude' => true,
-	'label' => &$GLOBALS['TL_LANG']['tl_module']['cajaxIdentifierString'],
-	'inputType' => 'text',
-	'eval' => array('tl_class'=>'long'),
-	'sql' => "varchar(255) NOT NULL default ''"
+    'exclude' => true,
+    'label' => &$GLOBALS['TL_LANG']['tl_module']['cajaxIdentifierString'],
+    'inputType' => 'text',
+    'eval' => array('tl_class'=>'long'),
+    'sql' => "varchar(255) NOT NULL default ''"
 );
