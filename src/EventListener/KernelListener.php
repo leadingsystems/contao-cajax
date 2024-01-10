614b96640023056d1fb5d397b9f61bf2d21281d4
Rainer Blechinger <57102323+superMi88@users.noreply.github.com>
2024-01-10 17:42:30 +0100

    fix: check if contao framework is initialized

--- src/EventListener/KernelListener.php
+++ src/EventListener/KernelListener.php
@@ -2,14 +2,28 @@
 
 namespace LeadingSystems\CajaxBundle\EventListener;
 
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
 use LeadingSystems\Cajax\ls_cajax_mainController;
 
 
 class KernelListener
 {
+    private $framework;
+
+    public function __construct(ContaoFramework $framework)
+    {
+        $this->framework = $framework;
+    }
+
     public function onKernelControllerArguments(ControllerArgumentsEvent $event)
     {
+
+        //contao framework is not initialized for images etc. so we can just return and dont get any errors
+        if (!$this->framework->isInitialized()) {
+            return;
+        }
+
         ls_cajax_mainController::getInstance()->receiveRequestData();
     }
 }
